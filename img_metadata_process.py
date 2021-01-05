7c323f8b4ac5f9566b81de5b82adaa5fc52eda2c
aiventures <49552433+aiventures@users.noreply.github.com>
2021-01-05 18:35:00 +0100

    Rewrite Process Images Method

--- img_metadata_process.py
+++ img_metadata_process.py
@@ -1,3 +1,4 @@
+""" Image Metadata Processing """
 import os
 from importlib import reload
 import win32clipboard
@@ -6,52 +7,39 @@ import image_meta
 import image_meta.controller
 reload(image_meta)
 reload(image_meta.controller)
+# Import classes
 from image_meta.controller import Controller
+# This will copy file path from your clipboard 
+# comment this in case you want to do it differently
+from tools.simple_input import v
 
 # --- EXECUTE METADATA RUN FOR IMAGES ---
 # * copy metadata.tpl and metadata_exif to target directory
 # * set default gps / in case track is used, adjust the GPS data as needed (defaultname tracks.gpx)
-# * in explorer simply copy path and start the script / alternatively paste the path from explorer address into clipboard
+# * in explorer simply copy path and start the script / alternatively paste the path into input
 
 #control_params
 showinfo = True # show info during execution
-verbose = True # Show detailed information
+verbose = False # Show detailed information
 ext_list = ["meta","geo"] # move / copy auxiliary files for given extensions after processing
-del_list = [*ext_list,"jpg_original","tif","arw.dop","dop","arw"] # same but list of extension for file types to be deleted
+del_list = [*ext_list,"jpg_original","tif","dop","xmp"] # list of file extensions that are supposed to be deleted
+del_src_ext="ARW" # extension that will be used to identify duplicates (in this case files starting with same name)
 persist = True # apply changes, eg copy files / delete files show upoming changes only otherwise
 metadata_subdir = "metadata" #subdirectory to store any generated metadata of interest
 metadata_file = "metadata.tpl" #default name of metadata file / is assumed to reside in working directory
 
-print("--- IMAGE METADATA PROCESSING ----")
+print("\n--- IMAGE METADATA PROCESSING ----")
 
 # get filepath either from clipboard, or from input, will be None if invalid filepath is given
-# filepaths will be determined in the following order:
-# 1 if valid filepath is given in code here
-# 2 valid filepath is copied into clipboard 
-# 3 valid filepath entered in input
-# 4 None if no valid filepath is given
+fp = v
+#fp = r"C:\<path to your directory>\_test_"
 
-fp = None
-fp = r"C:\<path to your directory>\_test_"
+print(f"    Filepath of WORKING DIRECTORY set to {fp} \n")
 
-if isinstance(fp,str) and not(os.path.isdir(fp)):
-    fp = None
-
-if fp is None:
-    # try to get path from clipboard
-    win32clipboard.OpenClipboard()
-    clipboard_data = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
-    
-    if isinstance(clipboard_data,str) and os.path.isdir(clipboard_data):
-        fp = clipboard_data
-    #get path 
-    else:
-        fp = input("Paste Filepath, hit any key to get path from config file\n --> ")
-        if not os.path.isdir(fp):
-            fp = None
-
-print(f"---- Filepath of WORKING DIRECTORY set to {fp} ----\n")
+if not os.path.isdir(fp):
+    print(f"    {fp} is not a valid path / press key to exit")
+    input("")
+    quit()
 
 if not(fp is None) and isinstance(metadata_subdir,str):
     copy_dir = os.path.join(fp,metadata_subdir)
@@ -70,7 +58,8 @@ if not(fp is None) and isinstance(metadata_file,str):
 if os.path.isfile(control_fp):
     finished = Controller.process_images(template_fileref=control_fp,showinfo=showinfo,
                                          verbose=verbose,copy_dir=copy_dir,copy_ext_list=ext_list,
-                                         del_ext_list=del_list,persist=persist,work_dir=fp)
+                                         del_ext_list=del_list, del_src_ext=del_src_ext, 
+                                         persist=persist,work_dir=fp)
     print(f"\nProcessing finished {finished}")
 else:
     print(f"Control file {control_fp} does not exist")
